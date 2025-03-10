06ba0dff3228b598ff2eda2fe42caaae5fa6a687
mruvezbrand <mruvez4@gmail.com>
2025-03-10 04:08:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def send_message():
     label { color: white; }
     .file { height: 30px; }
     body {
-      background-image: url('https://i.ibb.co/Rkvj0mcF/55d49315d84919105bd0c5b83c31ca54.jpg');
+      background-image: url('https://i.ibb.co/x8g7YMSM/27fbc22c11a7dab37e19ce183381c20c.jpg');
       background-size: cover;
       background-repeat: no-repeat;
       color: white;
